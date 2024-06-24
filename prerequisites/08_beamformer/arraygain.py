cac1a76fa3ff764cb924d45a97590b52d4240137
kasperikaivola <98049597+kasperikaivola@users.noreply.github.com>
2024-06-24 03:05:34 +0200

    Exer1 done, exer2 partially done (faulty)

--- prerequisites/08_beamformer/arraygain.py
+++ prerequisites/08_beamformer/arraygain.py
@@ -2,6 +2,7 @@ from tempfile import tempdir
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import sys
 
 # General Parameters:
 script_dir = os.path.dirname(__file__)
@@ -11,14 +12,23 @@ n = 8  # Number of sensors
 
 # Simulation parameters and settings
 # ==========================================
-setting = 1  # Configure different simulations
+setting = 1  # choose between 1-2
+d = 0.0425
+try:
+    #print(sys.argv[1])
+    setting = int(sys.argv[1])
+    d = float(sys.argv[2])
+except:
+    pass
 
 if setting == 1:
-    theta_0 = ...  # endfire array
-    d = ...  # Distance between adjacent sensors
-    title = ...
+    theta_0 = np.pi  # endfire array (180 degrees)
+    #d = ...  # Distance between adjacent sensors
+    title = ' Endfire Array'
 elif setting == 2:
-    ...
+    theta_0 = np.pi / 2  # broadside array (90 degrees)
+    #d = 4.25 / 100  # Distance between adjacent sensors (converted to meters)
+    title = ' Broadside Array'
     
 # dist = np.arange(n)*d  # last value is 0.6000001 instead of 0.6???
 dist = np.round(np.arange(n)*d, 4)  # Distances to reference microphone
@@ -34,8 +44,8 @@ th = -25  # db-threshold for plots, for better graphic representation
 # ======================
 # Calculating B(Omega)
 f_n = np.zeros((bf, n))
-tau = ... # todo
-k_n = ... # todo
+tau = dist / c * np.cos(theta_0)  # time delay
+k_n = tau * fs # todo
 for i in range(n):
     k = np.linspace(-bf2 + 1, bf2, bf)
     f_n[:, i] = np.sinc(k[:bf] - k_n[i])  # fractional delay filter
@@ -48,10 +58,12 @@ E = np.zeros((n,bf2))
 
 for f in range(bf2): # frequency index
     # Calculating array-steering vector with dimension 1xN
-    E = ... # todo
+    #E = np.exp(-1j * 2 * np.pi * freqs[f] * dist / c * np.cos(theta_0)) # todo
+    E[:, f] = np.exp(-1j * 2 * np.pi * freqs[f] * dist / c * np.cos(theta_0))  # Array steering vector
 
     # Calculating directivity pattern for target direction only (Matrix dimension is Bf2x1)
-    Psi[f] = ...
+    #Psi[f] = np.abs(np.dot(B[:, f], E.T))
+    Psi[f] = np.abs(np.dot(B[:, f], E[:, f].T))
 
 Bconj = np.conj(B).T
 
@@ -60,12 +72,14 @@ Psi_wn = np.zeros(bf2)
 arg = (2 * freqs * d / c).T
 
 for i in range(n):
-    Psi_wn = ...
+    #Psi_wn = ... #todo
+    Psi_wn += np.abs(B[i, :bf2]) ** 2  # White noise gain calculation
     for m in range(n):
-        Psi_diff = ...
+        #Psi_diff = ... #todo
+        Psi_diff += np.abs(np.exp(1j * arg * (i - m)))  # Difference gain calculation
 
-G = 10 * np.log10( ... )  # Calculating Array Gain
-Gwn = 10* np.log10( ... )  # Calculating White Noise Gain
+G = 10 * np.log10( np.abs(Psi) ** 2 / Psi_wn )  # Calculating Array Gain
+Gwn = 10* np.log10( Psi_diff / Psi_wn )  # Calculating White Noise Gain
 
 # optional but interesting
 f_aliasing = c/d*1/(1+np.cos(theta_0))
@@ -76,7 +90,7 @@ f_aliasing = c/d*1/(1+np.cos(theta_0))
 fig, axs = plt.subplots(2)
 
 axs[0].plot(freqs, G)
-axs[0].title.set_text('Array Gain ('+title+', d = ' + str(d) + 'cm), Aliasing above ' + str(round(f_aliasing)) + 'Hz')
+axs[0].title.set_text('Array Gain ('+title+', d = ' + str(d) + 'cm), Aliasing above ' + str(round(1)) + 'Hz')
 axs[0].set_xlabel('Frequency [Hz]')
 axs[0].set_ylabel('Gain [dB]')
 # think of meaningful y limits
@@ -88,5 +102,6 @@ axs[1].set_xlabel('Frequency [Hz]')
 axs[1].set_ylabel('Gain [dB]')
 axs[1].grid(True)
 # think of meaningful y limits
-plt.savefig(script_dir+'/arraygain_setting_'+str(setting)+'.png')
-plt.show()
+plt.savefig(script_dir+'/arraygain_setting_'+str(setting)+'_'+str(d)+'.png')
+#plt.show()
+plt.close()
--- prerequisites/08_beamformer/beampattern.py
+++ prerequisites/08_beamformer/beampattern.py
@@ -12,6 +12,7 @@ import numpy as np
 from matplotlib import cm
 from matplotlib.colors import LightSource
 import os
+import sys
 plt.ion() # is used to prevent the figures at the end from closing
 
 # General Parameters
@@ -23,13 +24,26 @@ fs = 16000  # Sampling frequency
 # Variable Parameters
 n = 8  # Number of sensors
 
-setting = 1  # Configure different simulations
+#setting = 1  # Configure different simulations
+
+# switch between exercises
+setting = 1  # choose between 1-2
+d = 4.25/100
+try:
+    #print(sys.argv[1])
+    setting = int(sys.argv[1])
+    d = float(sys.argv[2])
+except:
+    pass
 
 if setting == 1:
-    theta_0 = ...  # endfire array
-    d = ...  # Distance between adjacent sensors
+    theta_0 = np.pi  # endfire array (180 degrees)
+    #d = 4.25 / 100  # Distance between adjacent sensors
+    title = ' Endfire Array'
 elif setting == 2:
-    ...
+    theta_0 = np.pi / 2  # broadside array (90 degrees)
+    #d = 4.25 / 100  # Distance between adjacent sensors (converted to meters)
+    title = ' Broadside Array'
 
 theta_0_deg = theta_0/np.pi*180
 
@@ -37,13 +51,13 @@ dist = np.round(np.arange(n)*d, 4)  # Distances to reference microphone
 
 bw = 200  # number of discrete angles
 
-angles = ...  # Set of all angles (in Rad)
-angles_deg = ...  # ... in Deg
+angles = np.linspace(-np.pi, np.pi, bw) # Set of all angles (in Rad)
+angles_deg = angles / np.pi * 180  # ... in Deg
 
 bf = 256  # Number of all the discrete frequency points
 bf2 = int(round(bf / 2))  # rounded for better legibility
 
-freqs = ...  # Set of all discrete angles
+freqs = np.linspace(0, fs / 2, bf2)  # Set of all discrete angles
 #freqs = np.arange(0, fs / 2 + (fs / 2 ) * 1 / bf2, fs / 2 * 1 / bf2) # common mistake: np.arrange(0, fs/2, fs/2*1/bf2)
 th = -25  # db-threshold for plots, for better graphic representation
 
@@ -68,10 +82,13 @@ for a in range(bw):  # angle index
     for f in range(bf2):  # frequency index
         # Calculating array-steering vector with dimension 1xN
         # todo: E = ?
+        E = np.exp(-1j * 2 * np.pi * freqs[f] * dist / c * np.cos(angles[a]))
 
         # Calculating directivity pattern (Matrix dimension is Bf2xBw)
         # todo: Psi[f, a] =
+        Psi[f, a] = np.abs(np.dot(B[:, f], E.T))
         # todo: Psi_db[f, a] =
+        Psi_db[f, a] = 20 * np.log10(Psi[f, a])
 
         if Psi_db[f, a] < th:
             Psi_db[f, a] = th
@@ -85,23 +102,33 @@ for a in range(bw):  # angle index
 
 # 3 dimensional representation (surface plot)
 fig1 = plt.figure()
-ax1 = fig1.gca(projection='3d')
-grid_theta, grid_f = ...  # todo: coordinate mesh grid for surface plot
+ax1 = fig1.add_subplot(111, projection='3d')
+grid_theta, grid_f = np.meshgrid(angles_deg, freqs)  # todo: coordinate mesh grid for surface plot
 
 plot_args = dict(rcount=bf, ccount=bw, cmap=cm.jet, linewidth=1, antialiased=True)
 surf = ax1.plot_surface(grid_theta, grid_f, Psi_db, **plot_args)
+ax1.view_init(30, 60)
 # todo: set correct title and labels
-...
+ax1.set_title('3D Directivity Pattern, setting '+str(setting)+', '+str(title)+'sensor dist d='+str(d))
+ax1.set_xlabel('Angle (degrees)')
+ax1.set_xlim([-150,150])
+ax1.set_ylabel('Frequency (Hz)')
+ax1.set_ylim([0,8000])
+ax1.set_zlabel('Gain (dB)')
+ax1.set_zlim([-25,0])
 
-ax1.view_init(40, 120)
-plt.savefig(script_dir+'/3d_directivity_setting_'+str(setting)+'.png')
-plt.show()
-
+#ax1.view_init(40, 120)
+plt.savefig(script_dir+'/3d_directivity_setting_'+str(setting)+'sensor dist d='+str(d)+'.png')
+#plt.show()
 
 fig2 = plt.figure()
 ax2 = fig2.gca()
-plt.pcolormesh(...)  # todo plot 2D view
+pcol = plt.pcolormesh(grid_theta, grid_f, Psi_db, shading='auto', cmap=cm.jet)  # todo plot 2D view
+fig2.colorbar(pcol, ax=ax2)
 # todo: set correct title and labels
-...
+ax2.set_title('2D Directivity Pattern, setting '+str(setting)+', '+str(title)+'sensor dist d='+str(d))
+ax2.set_xlabel('Angle (degrees)')
+ax2.set_ylabel('Frequency (Hz)')
 plt.savefig(script_dir+'/2d_directivity_setting_'+str(setting)+'.png')
-plt.show()
+#plt.show()
+plt.close()
